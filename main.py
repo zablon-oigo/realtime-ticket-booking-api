ffe183667f63bce41aec2323a457fb382dae708e
zablon <zablonoigo51@gmail.com>
2025-06-27 17:13:57 +0300

    ad lifespan

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
+from src.db.core  import initdb
 app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await initdb() 
+    yield
+
+app = FastAPI(
+    title="Tickets Booking API",
+    description="A RealTime Ticket Booking RESTful API",
+    lifespan=lifespan
+)
 
 @app.get("/")
 def home():
