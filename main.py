5fc3b73c98aab6890fc89b0dce8ecda5273b5cb4
zablon <zablonoigo51@gmail.com>
2025-06-29 22:51:59 +0300

    Add auth routes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.db.core  import initdb
-app = FastAPI()
+from src.auth.routes import auth_router
 
+app = FastAPI()
+version = 'v1'
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await initdb() 
@@ -11,9 +13,10 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Tickets Booking API",
     description="A RealTime Ticket Booking RESTful API",
-    lifespan=lifespan
+    lifespan=lifespan,
+    version=version
 )
-
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
 @app.get("/")
 def home():
     return {"msg": "Hello, World!"}
