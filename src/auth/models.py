f0da8c4afd1e9d19b49cdce271e3a5f1e6b63700
zablon <zablonoigo51@gmail.com>
2025-08-10 23:37:21 +0300

    fixed default factory

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,27 +1,37 @@
 from __future__ import annotations
-from sqlmodel import SQLModel, Field, Column, Relationship
+from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
 from sqlalchemy.sql import func
 import uuid
 from datetime import datetime, timezone
-from typing import Optional, List
+from typing import Optional
+
 
 class User(SQLModel, table=True):
     __tablename__ = "user_accounts"
 
     uid: uuid.UUID = Field(
         default_factory=uuid.uuid4,
-        sa_column=Column(pg.UUID, primary_key=True, unique=True, nullable=False)
+        sa_column=Column(pg.UUID(as_uuid=True), primary_key=True, unique=True, nullable=False)
     )
     username: Optional[str] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
-    role: str = Field(sa_column=Column(pg.VARCHAR, nullable=False, server_default="user"))
-    is_verified: bool = False
-    email: str = Field(sa_column=Column(pg.VARCHAR(40), unique=True, nullable=False))
+    role: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
+    )
+    is_verified: bool = Field(
+        default=False,
+        sa_column=Column(pg.BOOLEAN, nullable=False, server_default="false")
+    )
+    email: str = Field(
+        sa_column=Column(pg.VARCHAR(40), unique=True, nullable=False)
+    )
     password_hash: str
-    created_at: datetime = Field(default_factory=datetime.now(timezone.utc), sa_column=Column(pg.TIMESTAMP, default=func.now()))
-
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(pg.TIMESTAMP(timezone=True), server_default=func.now())
+    )
 
     def __init__(self, **data):
         if "username" not in data or not data["username"]:
