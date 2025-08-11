ffc2321be19236d204857baf189c16a1089973db
zablon <zablonoigo51@gmail.com>
2025-08-11 22:37:58 +0300

    add users relationship with bookings model

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
-from sqlmodel import SQLModel, Field, Column
+from sqlmodel import SQLModel, Field, Column, Relationship
 import sqlalchemy.dialects.postgresql as pg
 from sqlalchemy.sql import func
 import uuid
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, List
 
 
 class User(SQLModel, table=True):
@@ -17,21 +17,17 @@ class User(SQLModel, table=True):
     username: Optional[str] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
-    role: str = Field(
-        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
-    )
-    is_verified: bool = Field(
-        default=False,
-        sa_column=Column(pg.BOOLEAN, nullable=False, server_default="false")
-    )
-    email: str = Field(
-        sa_column=Column(pg.VARCHAR(40), unique=True, nullable=False)
-    )
+    role: str = Field(sa_column=Column(pg.VARCHAR, nullable=False, server_default="user"))
+    is_verified: bool = Field(default=False, sa_column=Column(pg.BOOLEAN, nullable=False, server_default="false"))
+    email: str = Field(sa_column=Column(pg.VARCHAR(40), unique=True, nullable=False))
     password_hash: str
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
         sa_column=Column(pg.TIMESTAMP(timezone=True), server_default=func.now())
     )
+    events: List["Event"] = Relationship(back_populates="owner_user")
+    bookings: List["Booking"] = Relationship(back_populates="user")
+    tickets: List["Ticket"] = Relationship(back_populates="booked_by_user")
 
     def __init__(self, **data):
         if "username" not in data or not data["username"]:
