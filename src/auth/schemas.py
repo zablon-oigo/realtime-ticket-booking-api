8a247fa7d932509ed89135b3545806c5c90b4dd5
zablon <zablonoigo51@gmail.com>
2025-08-17 23:11:01 +0300

    reduce repetition adding base user

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -2,20 +2,19 @@ from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
 
 
-class UserCreateModel(BaseModel):
+class BaseUserModel(BaseModel):
+    email: EmailStr = Field(max_length=40)
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreateModel(BaseUserModel):
     first_name: Optional[str] = Field(default=None, max_length=15)
     last_name: Optional[str] = Field(default=None, max_length=15)
     username: Optional[str] = Field(default=None, max_length=10)
-    email: EmailStr = Field(max_length=40)
     password: str = Field(min_length=6, max_length=128)
 
-    class Config:
-        orm_mode = True
 
-
-class UserLoginModel(BaseModel):
-    email: EmailStr = Field(max_length=40)
+class UserLoginModel(BaseUserModel):
     password: str = Field(min_length=6, max_length=128)
-
-    class Config:
-        orm_mode = True
